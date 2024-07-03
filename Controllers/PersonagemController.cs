8f14768b5236575a8012bb7a7f949b5529fe97f6
Sabrina Santos da Silva <sabrina.silva@sesisenaisp.org.br>
2024-07-03 16:54:12 -0300

    feat: tela de criação de personagens

---
+++ Controllers/PersonagemController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AiRpgFrontEnd.Controllers
+{
+    public class PersonagemController : Controller
+    {
+        private readonly ILogger<PersonagemController> _logger;
+
+        public PersonagemController(ILogger<PersonagemController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
