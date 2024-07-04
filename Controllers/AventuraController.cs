e75ba4094f53db1e97c62da42286b15b6f7361eb
Gustavo Dionizio <gustavo.dionizio@sesisenaisp.org.br>
2024-07-04 10:37:30 -0300

    push to pull

--- Controllers/AventuraController.cs
+++ Controllers/AventuraController.cs
@@ -4,9 +4,28 @@ namespace AiRpgFrontEnd.Controllers
 {
     public class AventuraController : Controller
     {
+        private readonly Uri apiUri = new("http://127.0.0.1:5000");
+        private readonly HttpClient _client;
+
+        public AventuraController ()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = apiUri
+            };
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<string> CriarHistoria()
+        {
+            HttpResponseMessage resposta = await _client.GetAsync(apiUri + "/create_session");
+
+            return "";
         }
     }
 }
