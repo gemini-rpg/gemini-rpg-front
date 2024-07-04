3e83abee4d93e8ab5d69599899c8b43cfb6d1907
Sabrina Santos da Silva <sabrina.silva@sesisenaisp.org.br>
2024-07-04 08:35:24 -0300

    adicionando index e controller aventura

---
+++ Controllers/AventuraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AiRpgFrontEnd.Controllers
+{
+    public class AventuraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
