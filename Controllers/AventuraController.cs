bc4af5893bfad6e32afa9bb53390ab9a44a7cffa
Gustavo Dionizio <gustavo.dionizio@sesisenaisp.org.br>
2024-07-10 10:32:19 -0300

    fix: atualiza a voz sintetizada

--- Controllers/AventuraController.cs
+++ Controllers/AventuraController.cs
@@ -14,8 +14,10 @@ namespace AiRpgFrontEnd.Controllers
     {
 
         private readonly Uri apiUri = new("http://127.0.0.1:5000");
+        private static string speechKey = "a46b765bbdcf4bc29363c79159952404";
+        private static string speechRegion = "brazilsouth";
         private readonly HttpClient _client;
-
+        private static readonly SpeechSynthesizer fala = new(SpeechConfig.FromSubscription(speechKey, speechRegion));
         public AventuraController()
         {
             _client = new HttpClient
@@ -86,6 +88,7 @@ namespace AiRpgFrontEnd.Controllers
 
                     var hist = historia.message;
                     ViewBag.Historia = FormataHistoria(hist);
+                    TempData["Historia"] = ViewBag.Historia;
                     ViewBag.Opcoes = FormataOpcoes(hist);
                     ViewBag.IdSessao = historia.chat_id;
                     ViewBag.Nome = model.nome;
@@ -100,37 +103,33 @@ namespace AiRpgFrontEnd.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> PostSpeech()
+        public async Task<IActionResult> PostSpeech(string historiaTexto)
         {
             // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-            string speechKey = Environment.GetEnvironmentVariable("SPEECH_KEY");
-            string speechRegion = Environment.GetEnvironmentVariable("SPEECH_REGION");
-
-            string text = TempData["Historia"].ToString();
 
             try
             {
                 var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
                 speechConfig.SpeechSynthesisVoiceName = "pt-BR-ElzaNeural";
 
-                using (var speechSynthesizer = new SpeechSynthesizer(speechConfig))
+                //check if there is audio playing, and if there is, stop it
+                await fala.StopSpeakingAsync();
+
+                var speechSynthesisResult = await fala.SpeakTextAsync(historiaTexto);
+
+                // OutputSpeechSynthesisResult(speechSynthesisResult, text); // Pode não ser necessário para o retorno HTTP
+
+                // Verifica o resultado da síntese de fala
+                if (speechSynthesisResult.Reason == ResultReason.SynthesizingAudioCompleted)
                 {
-                    var speechSynthesisResult = await speechSynthesizer.SpeakTextAsync(text);
-
-                    // OutputSpeechSynthesisResult(speechSynthesisResult, text); // Pode não ser necessário para o retorno HTTP
-
-                    // Verifica o resultado da síntese de fala
-                    if (speechSynthesisResult.Reason == ResultReason.SynthesizingAudioCompleted)
-                    {
-                        // Se a síntese foi completada com sucesso, retornar um OkResult
-                       return Ok(new { Status = "Completed", Text = text });
-                    }
-                    else
-                    {
-                        // Se houve cancelamento ou erro na síntese
-                        var cancellation = SpeechSynthesisCancellationDetails.FromResult(speechSynthesisResult);
-                        return BadRequest($"Speech synthesis failed. Reason={cancellation.Reason}, ErrorCode={cancellation.ErrorCode}, ErrorDetails={cancellation.ErrorDetails}");
-                    }
+                    // Se a síntese foi completada com sucesso, retornar um OkResult
+                    return Ok(new { Status = "Completed", Text = historiaTexto });
+                }
+                else
+                {
+                    // Se houve cancelamento ou erro na síntese
+                    var cancellation = SpeechSynthesisCancellationDetails.FromResult(speechSynthesisResult);
+                    return BadRequest($"Speech synthesis failed. Reason={cancellation.Reason}, ErrorCode={cancellation.ErrorCode}, ErrorDetails={cancellation.ErrorDetails}");
                 }
             }
             catch (Exception ex)
